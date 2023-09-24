f92ecfab43f2a491884336cef2bcd555a48970fe
advanced-user <stolbetskiy.yura@gmail.com>
2023-09-24 15:58:15 +0300

    docker compose

--- server_test/src/main.rs
+++ server_test/src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, App, HttpResponse, HttpServer};
 
 #[get("/hello")]
 pub async fn test_hello() -> HttpResponse {
-    HttpResponse::Ok().json("Hello world")
+    HttpResponse::Ok().json("Hello world\n")
 }
 
 #[actix_web::main]
