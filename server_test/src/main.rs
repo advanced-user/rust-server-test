31359250549969da6127924210375b70e0837ee2
advanced-user <stolbetskiy.yura@gmail.com>
2023-09-24 11:55:57 +0300

    test deploy 9000

--- server_test/src/main.rs
+++ server_test/src/main.rs
@@ -8,7 +8,7 @@ pub async fn test_hello() -> HttpResponse {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(move || App::new().service(test_hello))
-        .bind("localhost:8080")?
+        .bind("localhost:9000")?
         .run()
         .await
 }
